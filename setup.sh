65fe7ec156085f6330b16ae542db3bdd00d78cdf
Tomoyuki Sakurai <y@trombik.org>
2024-05-16 19:34:30 +0700

    bugfix: remove unused symlink

--- setup.sh
+++ setup.sh
@@ -8,7 +8,3 @@ fi
 if [ ! -d "${LANGUAGETOOL_DIR}" ]; then
     unzip "${LANGUAGETOOL_FILE_ZIP}"
 fi
-
-if [ ! -L "${LANGUAGETOOL_DIR}/${LANGUAGETOOL_DIR_RULES}/${CUSTOM_GRAMMAR_FILE_NAME}" ]; then
-    ( cd "${LANGUAGETOOL_DIR}/${LANGUAGETOOL_DIR_RULES}" && ln -s "../../../../../${CUSTOM_GRAMMAR_FILE_NAME}" "${CUSTOM_GRAMMAR_FILE_NAME}" )
-fi
